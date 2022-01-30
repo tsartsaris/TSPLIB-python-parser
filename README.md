bfde27f411737638ae22aaacae201121de6631a3
stsartsaris <s.tsartsaris@delivery.gr>
2022-01-30 22:56:05 +0200

    - fixed issue with coordinates been with unequal spaces " 1   23.45     45  " - all coordinates are now passed as a float - fixed issue with negative coordinates - moved from Functional Programming to Object-Oriented Programming. use a class now, will expand the parser with strategies for other TSP problem types (HCP, ATSP etc...)

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 TSPLIB-python-parser
 ====================
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://GitHub.com/Naereen/StrapDown.js/graphs/commit-activity)
 
-This is a parser to read tsplib problems data into python list and dict
+
+This is a parser to read 2D Symmetric traveling salesman problem STSP problems data into python dict
 so from a file like 
 
+## How to use it
+- command line 
+```commandline
+parser.py --file <filename> --plot true
+```
+- from python script 
+```python 
+TSPParser(filename=f, plot_tsp=p)
+```
+
+## What it does
+
+feed it with a file like
+
+(***~~cheat~~***)
+```commandline
 NAME: ulysses16.tsp
 TYPE: TSP
 COMMENT: Odyssey of Ulysses (Groetschel/Padberg)
@@ -28,17 +48,21 @@ NODE_COORD_SECTION
  15 35.49 14.32
  16 39.36 19.56
  EOF
+``` 
  
- 
- you will end up having a dict like
- 
- [(1, ('38.24', '20.42')), (2, ('39.57', '26.15')), 
- (3, ('40.56', '25.32')), (4, ('36.26', '23.12')), 
- (5, ('33.48', '10.54')), (6, ('37.56', '12.19')), 
- (7, ('38.42', '13.11')), (8, ('37.52', '20.44')), 
- (9, ('41.23', '9.10')), (10, ('41.17', '13.05')), 
- (11, ('36.08', '-5.21')), (12, ('38.47', '15.13')), 
- (13, ('38.15', '15.35')), (14, ('37.51', '15.17')), 
- (15, ('35.49', '14.32')), (16, ('39.36', '19.56'))]
- 
- As an addon you plot the cities with matplotlib and the current rout
+you will end up having a dict like
+
+```python 
+{'1': (38.24, 20.42), '2': (39.57, 26.15),
+'3': (40.56, 25.32), '4': (36.26, 23.12),
+'5': (33.48, 10.54), '6': (37.56, 12.19),
+'7': (38.42, 13.11), '8': (37.52, 20.44),
+'9': (41.23, 9.1), '10': (41.17, 13.05),
+'11': (36.08, -5.21), '12': (38.47, 15.13),
+'13': (38.15, 15.35), '14': (37.51, 15.17),
+'15': (35.49, 14.32), '16': (39.36, 19.56)}
+ ```
+
+***As an addon you plot the cities with matplotlib and the current route based on the order in the initial file***
+
+![](tsp_plot.jpg)
--- parser.py
+++ parser.py
@@ -1,132 +1,31 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-import re
-import matplotlib.pyplot as plt
 import argparse
 
-#  We use a regex here to clean characters and keep only numerics
-
-cities_set = []
-cities_tups = []
-cities_dict = {}
-#  we open the TSP file and put each line cleaned of spaces
-#  and newline characters in a list 
-def read_tsp_data(tsp_name):
-	tsp_name = tsp_name
-	with open(tsp_name) as f:
-		content = f.read().splitlines()
-		cleaned = [x.lstrip() for x in content if x != ""]
-		return cleaned
-"""
-We return a list like 
-['NAME: ulysses16.tsp',
-'TYPE: TSP',
-'COMMENT: Odyssey of Ulysses (Groetschel/Padberg)',
-'DIMENSION: 16',
-'EDGE_WEIGHT_TYPE: GEO',
-'DISPLAY_DATA_TYPE: COORD_DISPLAY',
-'NODE_COORD_SECTION',
-'1 38.24 20.42',
-'2 39.57 26.15',
-'3 40.56 25.32',
-................
-'EOF']
-"""
-
-"""
-Check for the line DIMENSION in the file and keeps the numeric value
-"""
-def detect_dimension(in_list):
-	non_numeric = re.compile(r'[^\d]+')
-	for element in in_list:
-		if element.startswith("DIMENSION"):
-			return non_numeric.sub("",element)
-
-"""
-Iterate through the list of line from the file
-if the line starts with a numeric value within the 
-range of the dimension , we keep the rest which are
-the coordinates of each city
-1 33.00 44.00 results to 33.00 44.00
-"""
-def get_cities(list,dimension):
-	dimension = int(dimension)
-	for item in list:
-		for num in range(1, dimension + 1):
-			if item.startswith(str(num)):
-				index, space, rest = item.partition(' ')
-				if rest not in cities_set:
-					cities_set.append(rest)
-	return cities_set
+from tsplib_parser.tsp_file_parser import TSPParser
 
 
-"""
-Brake each coordinate 33.00 44.00 to a tuple ('33.00','44.00')
-"""
-def city_tup(list):
-	for item in list:
-		first_coord, space, second_coord = item.partition(' ')
-		cities_tups.append((first_coord.strip(), second_coord.strip()))
-	return cities_tups
+def parse_boolean(value):
+    value = value.lower()
 
-"""
-We zip for reference each city to a number
-in order to work and solve the TSP we need a list 
-of cities like 
-[1,2,3,4,5,...........]
-with the dictionary we will have a reference of the coordinates of each city 
-to calculate the distance within (i + 1, i) or (2 - 1) were 2 and 1 represents each city
-"""
-def create_cities_dict(cities_tups):
-	return zip((range(1,len(cities_tups)+1)),cities_tups)
+    if value in ["true", "yes", "y", "1", "t"]:
+        return True
+    elif value in ["false", "no", "n", "0", "f"]:
+        return False
 
-"""
-Just to plot the results
-"""
-def plot_cities(cities_tups):
-	plt.clf()
-	plt.scatter(*zip(*cities_tups))
-	plt.plot(*zip(*cities_tups))
-	plt.show()
-
-
-"""
-Putting it all together
-"""
-def produce_final(file="ulysses16.tsp"):
-	data = read_tsp_data(file)
-	dimension = detect_dimension(data)
-	cities_set = get_cities(data,dimension)
-	cities_tups = city_tup(cities_set)
-	cities_dict = create_cities_dict(cities_tups)
-	plot_cities(cities_tups)
-	print (str(cities_dict))
-	
-
+    return False
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f","--file",help="tsp file to plot.",type=str,nargs=1)
+    parser.add_argument("--file", help="tsp file to parse.", type=str)
+    parser.add_argument("--plot", help="plot the file.", default=False, type=parse_boolean)
     args = parser.parse_args()
     f = args.file
-    if f != None :
-        produce_final(f[0])
-    else :
-        produce_final()
-#    # or produce_final("berlin52.tsp") or whatever filename you wish to have
+    p = args.plot
+    if f is not None:
+        TSPParser(filename=f, plot_tsp=p)
+    else:
+        print("Please provide a file")
+
+
+
+# d57b652c-7f1b-4713-9ae1-4cac5f06874b
